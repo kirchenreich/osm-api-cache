546736bae4661e78dcb69d0c8453f9043aaabf22
Andreas Madsack <andreas@madflex.de>
2014-09-06 21:28:31 +0200

    add credentials to readme and a test for test credentials

--- README.md
+++ README.md
@@ -11,6 +11,21 @@ if your application is not using the full osm database.
 * Python 3.3+
 * see requirements.txt
 
+## settings
+
+add the following settings to `credentials.py`
+
+```
+POSTGRES_USERNAME = 'osmcache'
+POSTGRES_DBNAME = 'osmcache'
+POSTGRES_PASSWORD = 'password'
+
+TEST_POSTGRES_USERNAME = 'test_osmcache'
+TEST_POSTGRES_DBNAME = 'test_osmcache'
+TEST_POSTGRES_PASSWORD = 'password'
+```
+
+
 ## usage
 
 ### help
--- tests/test_configuration_set.py
+++ tests/test_configuration_set.py
@@ -5,3 +5,10 @@ def test_if_configuration_is_set():
     assert hasattr(credentials, 'POSTGRES_USERNAME')
     assert hasattr(credentials, 'POSTGRES_PASSWORD')
     assert hasattr(credentials, 'POSTGRES_DBNAME')
+
+
+def test_if_test_configuration_is_set():
+    import credentials
+    assert hasattr(credentials, 'TEST_POSTGRES_USERNAME')
+    assert hasattr(credentials, 'TEST_POSTGRES_PASSWORD')
+    assert hasattr(credentials, 'TEST_POSTGRES_DBNAME')
